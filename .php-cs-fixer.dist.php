85037f08661d8abc6912f4049e56d8bbb6d56881
Volodymyr Dzhuryn <volodymyr.dzhuryn@otakoyi.com>
2024-10-06 18:43:27 +0300

    deploy for tickets

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,12 +3,15 @@
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('var')
+    ->exclude('vendor')
 ;
+
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         '@PSR12' => true,
     ])
+    ->setCacheFile('./var/.phpunit.cache')
     ->setFinder($finder)
 ;
